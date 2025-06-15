311aaf68809207f55db977f25c33d1fd126454a4
Karim Elsayad <karimelsayad1999@gmail.com>
2025-06-16 00:54:55 +0300

    Add Loggin out

--- easygen-frontend/src/routes/_authenticatedLayout/index.tsx
+++ easygen-frontend/src/routes/_authenticatedLayout/index.tsx
@@ -1,17 +1,36 @@
 import { useAuth } from '@/contexts/auth/useAuth.ts';
+import { Button } from '@/components/ui/Button.tsx';
+import { useNavigate } from '@tanstack/react-router';
 
 export const Route = createFileRoute({
   component: IndexComponent,
 });
 
 function IndexComponent() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await signOut();
+    navigate({ to: '/sign-in' });
+  };
 
   return (
     <div className="p-2">
-      <h1 className={"text-4xl"}>Welcome to the application.</h1>
-      <p className={"text-lg"}>You are {user?.name}</p>
-      <p>Your email is {user?.email}</p>
+      <h1 className={'text-4xl'}>Welcome to the application.</h1>
+      <p className={'text-lg mt-4'}>
+        You are <span className={'font-bold'}>{user?.name}</span>
+      </p>
+      <p className={'mt-2'}>
+        Your email is <span className={'font-bold'}>{user?.email}</span>
+      </p>
+      <Button
+        className={'mt-8 cursor-pointer'}
+        variant={'default'}
+        onClick={logout}
+      >
+        Log out
+      </Button>
     </div>
   );
 }
